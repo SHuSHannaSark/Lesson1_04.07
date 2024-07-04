483c186ea90b6d6ab84294cfa30193c5712b8aeb
SHuSHannaSark <shushanna@MacBook-Pro-Shushanna.local>
2024-07-05 00:40:39 +0300

    git add some variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+double d = 5.55;
+Console.WriteLine(d);
+char c = '!';
+Console.WriteLine(c);
+bool b = true;
+Console.WriteLine(b);
